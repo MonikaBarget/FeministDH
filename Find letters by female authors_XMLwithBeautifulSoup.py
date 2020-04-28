65c7b1dea36947beff13fd7ccd78e6b2cb504b88
Monika Barget <monika.barget@mu.ie>
2020-04-28 13:24:09 +0200

    Python script for identifying letters by gender
    This Python script was used to select all letters from a corpus of XML files that were written by women. The information on the authors' gender was stored in the Letters 1916-1923 metadata. Unfortunately, most XML files would not parse because transcriptions also stored in the files had been created by multiple volunteers and still required cleaning. This is why the Python package Beautiful Soup, written for XML as well as HTML, was used.

---
+++ Find letters by female authors_XMLwithBeautifulSoup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+from bs4 import BeautifulSoup
+import os
+from os.path import join
+import shutil
+
+directory=("C:\\Users\\mbarg\\Documents\\corpus") # directory containing all XML files
+
+results=[] # new result list
+item=("Female") # item searched for in XML
+for infile in os.listdir(directory):
+    filename=join(directory, infile)
+    indata=open(filename,"r", encoding="utf-8", errors="ignore") 
+    contents = indata.read()
+    soup = BeautifulSoup(contents,'xml')
+    keywords = soup.find_all('keywords') # accessing XML tag where gender info is stored
+    for keyword in keywords:
+        items=str(keyword.get_text())
+        if item in items:
+            results.append(infile) # add name of XML file to result list
+            shutil.copy2(join(directory, infile), 'C:\\Users\\mbarg\\Documents\\corpus_women') # copy file to subcorpus 
+print(results) # print list of file names in subcorpus
+print(len(results)) # print number of files in subcorpus
+
+
+# In[ ]:
+
+
+
+

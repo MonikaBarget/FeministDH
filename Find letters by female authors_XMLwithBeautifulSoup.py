c11c8d2681b28128882a623dbf9a2d0a6d2ddb00
Monika Barget <monika.barget@mu.ie>
2020-04-30 12:19:06 +0200

    Update Find letters by female authors_XMLwithBeautifulSoup.py

--- Find letters by female authors_XMLwithBeautifulSoup.py
+++ Find letters by female authors_XMLwithBeautifulSoup.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[10]:
-
-
 from bs4 import BeautifulSoup
 import os
 from os.path import join
@@ -12,7 +9,7 @@ import shutil
 directory=("C:\\Users\\mbarg\\Documents\\corpus") # directory containing all XML files
 
 results=[] # new result list
-item=("Female") # item searched for in XML
+items=["Female"] # items searched for in XML
 for infile in os.listdir(directory):
     filename=join(directory, infile)
     indata=open(filename,"r", encoding="utf-8", errors="ignore") 
@@ -26,10 +23,3 @@ for infile in os.listdir(directory):
             shutil.copy2(join(directory, infile), 'C:\\Users\\mbarg\\Documents\\corpus_women') # copy file to subcorpus 
 print(results) # print list of file names in subcorpus
 print(len(results)) # print number of files in subcorpus
-
-
-# In[ ]:
-
-
-
-

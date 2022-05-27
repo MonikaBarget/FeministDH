15456ad135fa4e2ba2110f929942425ab3da9966
Monika Barget <38257338+MonikaBarget@users.noreply.github.com>
2022-05-27 10:21:23 +0200

    v2

---
+++ qgis2web_2022_05_27-10_19_09_680795/data/IWHSOregionaldepots_6.js
@@ -0,0 +1,9 @@
+var json_IWHSOregionaldepots_6 = {
+"type": "FeatureCollection",
+"name": "IWHSOregionaldepots_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Source": "Derry Journal 1844-1924, Monday, December 13, 1915; Page: 3 \/ Derry Journal 1844-1924, Wednesday, February 23, 1916; Page: 2 \/ Derry Journal 1844-1924, Friday, November 24, 1916; Page: 7", "Unit no.": "none", "Function": "regional moss depot for the NORTH WEST OF IRELAND", "Work party \/ supply depot address": "STRAND HOUSE", "Town": "Derry \/ Londonderry", "Full Address": "Londonderry Ireland", "County (ancient)": "#", "County (modern)": "#", "Opened": "[01\/01\/1916]", "Closed": [ 1919 ], "Established \/ run by (last name)": "Lily E. Anderson (Mayoress) \/ the Duchess of Abercorn", "First and middle names": "#", "Gender": "#", "Marital status": "#", "Collaborators": "#" }, "geometry": { "type": "Point", "coordinates": [ -7.3085748, 54.9966124 ] } },
+{ "type": "Feature", "properties": { "Source": "Connacht Tribune 1909-current, Saturday, September 09, 1916; Page: 8", "Unit no.": "none", "Function": "regional supply depot", "Work party \/ supply depot address": "house to be taken", "Town": "Galway", "Full Address": "Galway Ireland ", "County (ancient)": "Galway", "County (modern)": "#", "Opened": "Sept. 1916", "Closed": [ 1919 ], "Established \/ run by (last name)": "Lady Ashtown", "First and middle names": "#", "Gender": "f", "Marital status": "married", "Collaborators": "#" }, "geometry": { "type": "Point", "coordinates": [ -9.0567905, 53.270668 ] } }
+]
+}
